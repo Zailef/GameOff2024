05f2241208bdcd8c0b4aaab65d3bfa81943db737
Tom <zailef.dev@hotmail.com>
2024-11-27 20:53:52 +0000

    Add a smoother fade-in animation for fade platforms

--- scenes/environment/platforms/fade_platform.gd
+++ scenes/environment/platforms/fade_platform.gd
@@ -3,18 +3,39 @@ class_name FadePlatform
 
 @onready var player_detection_area: Area3D = $PlayerDetectionArea
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var fade_timer: Timer = $FadeTimer
 
 var is_platform_revealed: bool = false
+var is_player_on_platform: bool = false
 
 func _ready() -> void:
 	player_detection_area.body_entered.connect(_on_player_detection_area_body_entered)
+	player_detection_area.body_exited.connect(_on_player_detection_area_body_exited)
+	fade_timer.timeout.connect(_on_fade_timer_timeout)
 
 func _on_player_detection_area_body_entered(body: Node) -> void:
 	if body is Player:
+		is_player_on_platform = true
+
 		if not is_platform_revealed:
 			is_platform_revealed = true
 			animation_player.play("fade_in")
 			await animation_player.animation_finished
 			material.distance_fade_mode = material.DISTANCE_FADE_DISABLED
 			material.transparency = material.TRANSPARENCY_DISABLED
-			player_detection_area.monitoring = false
+
+func _on_player_detection_area_body_exited(body: Node) -> void:
+	if body is Player and is_platform_revealed:
+		is_player_on_platform = false
+		fade_timer.start()
+
+func _on_fade_timer_timeout() -> void:
+	if not is_platform_revealed or is_player_on_platform:
+		return
+
+	is_platform_revealed = false
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
+	material.distance_fade_mode = material.DISTANCE_FADE_PIXEL_ALPHA
+	material.transparency = material.TRANSPARENCY_ALPHA
+	# material.transparency = 0.0
