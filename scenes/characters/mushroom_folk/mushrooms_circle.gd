e46c672b75dd7bcdb529db791a4735837c5e4afc
Tom <zailef.dev@hotmail.com>
2024-11-30 17:24:29 +0000

    Add and integrate mushroom greeting

--- scenes/characters/mushroom_folk/mushrooms_circle.gd
+++ scenes/characters/mushroom_folk/mushrooms_circle.gd
@@ -3,10 +3,15 @@ class_name MushroomCircle
 
 @onready var mushroom_watch_area: Area3D = $MushroomWatchArea
 @onready var mushrooms: Node3D = $Mushrooms
+@onready var audio_players: Node3D = $AudioPlayers
 
 func _ready() -> void:
 	mushroom_watch_area.body_entered.connect(_on_mushroom_watch_area_body_entered)
 	mushroom_watch_area.body_exited.connect(_on_mushroom_watch_area_body_exited)
+
+	for audio_player in audio_players.get_children():
+		audio_player = audio_player as AudioStreamPlayer3D
+		audio_player.play()
 
 	set_process(false)
 	set_physics_process(false)
