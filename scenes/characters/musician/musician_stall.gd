c17d3e80cceffdcc061f3b18c515973e495cc46f
Tom <zailef.dev@hotmail.com>
2024-11-30 20:01:38 +0000

    Treat the 3d audio music player as a regular one after the musician's stall is discovered

--- scenes/characters/musician/musician_stall.gd
+++ scenes/characters/musician/musician_stall.gd
@@ -5,12 +5,14 @@ class_name MusicianStall
 @onready var interaction_handler: InteractionHandler = $XylophoneMemoryGame/InteractionHandler
 @onready var xylophone_memory_game: XylophoneMemoryGame = $XylophoneMemoryGame
 @onready var audio_stream_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
-@onready var original_audio_stream_player_volume_db: float = audio_stream_player.volume_db
+@onready var player_discovery_area: Area3D = $PlayerDiscoveryArea
 
+@export var standard_volume_db: float = -2
 @export var mini_game_volumne_reduction_db: float = -5
 
 func _ready() -> void:
 	interaction_handler.interaction_started.connect(_on_interaction_started)
+	player_discovery_area.body_entered.connect(_on_player_discovery_area_body_entered)
 	xylophone_memory_game.game_started.connect(_on_game_started)
 	xylophone_memory_game.game_ended.connect(_on_game_ended)
 
@@ -23,4 +25,9 @@ func _on_game_started() -> void:
 	audio_stream_player.volume_db = mini_game_volumne_reduction_db
 
 func _on_game_ended() -> void:
-	audio_stream_player.volume_db = original_audio_stream_player_volume_db
+	audio_stream_player.volume_db = standard_volume_db
+
+func _on_player_discovery_area_body_entered(body: Node) -> void:
+	if body is Player:
+		audio_stream_player.attenuation_model = AudioStreamPlayer3D.AttenuationModel.ATTENUATION_DISABLED
+		audio_stream_player.volume_db = standard_volume_db
