45c09de3e3636b5025c94257f3d4149c5d1ea4d5
Tom <zailef.dev@hotmail.com>
2024-12-01 13:50:26 +0000

    Add highlight shaders for spawn cards and major arcana pickups

--- scenes/environment/platforms/spawn_card.gd
+++ scenes/environment/platforms/spawn_card.gd
@@ -3,7 +3,7 @@ class_name SpawnCard
 
 @onready var player_detection_area: Area3D = $PlayerDetectionArea
 @onready var spawn_point: Marker3D = $SpawnPoint
-@onready var activation_light: OmniLight3D = $ActivationLight
+@onready var card: CSGBox3D = $Card
 
 @export var is_active_spawn: bool = false
 
@@ -11,11 +11,13 @@ func _ready() -> void:
 	player_detection_area.body_entered.connect(_on_player_detection_area_body_entered)
 
 func _on_player_detection_area_body_entered(body: Node) -> void:
-	if body is Player:
-		is_active_spawn = true
-		activation_light.show()
-		
-		for child in get_tree().get_nodes_in_group("spawn_cards"):
-			if child != self:
-				child.is_active_spawn = false
-				child.activation_light.hide()
+	if not body is Player:
+		return
+
+	is_active_spawn = true
+	card.material_overlay.set_shader_parameter("is_active", true)
+	
+	for child in get_tree().get_nodes_in_group("spawn_cards"):
+		if child != self:
+			child.is_active_spawn = false
+			child.card.material_overlay.set_shader_parameter("is_active", false)
--- scenes/pickups/major_arcana_card.gd
+++ scenes/pickups/major_arcana_card.gd
@@ -6,15 +6,18 @@ const ANIMATION_FADE_OUT: String = "fade_out"
 @export var card_name: String
 @export var card_texture: Texture
 
-@onready var interaction_handler: InteractionHandler = $InteractionHandler
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pickup_area: Area3D = $PickupArea
 @onready var card_model: CSGBox3D = $CardModel
 
 func _ready() -> void:
-	interaction_handler.interaction_started.connect(_on_interaction_started)
 	card_model.material.normal_texture = card_texture
+	pickup_area.body_entered.connect(_on_pickup_area_body_entered)
 
-func _on_interaction_started() -> void:
+func _on_pickup_area_body_entered(body: Node3D) -> void:
+	if not body is Player:
+		return
+	
 	animation_player.play(ANIMATION_FADE_OUT)
 	SignalManager.major_arcana_card_collected.emit(card_name)
 	await animation_player.animation_finished
