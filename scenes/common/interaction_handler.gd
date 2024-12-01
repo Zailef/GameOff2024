0171cb5f10e3368e6a9a75881f536a517b9e5b93
Tom <zailef.dev@hotmail.com>
2024-12-01 02:22:12 +0000

    Make the interaction handler more flexible by having the label as an export

--- scenes/common/interaction_handler.gd
+++ scenes/common/interaction_handler.gd
@@ -4,12 +4,10 @@ class_name InteractionHandler
 
 signal interaction_started
 
-@onready var interaction_label: Label3D = $InteractionLabel
-@export var label_text = "Press [E] to interact"
+@export var interaction_label: Label3D
 
 func _ready() -> void:
 	interaction_label.visible = false
-	interaction_label.text = label_text
 
 	area_entered.connect(_on_interaction_area_entered)
 	area_exited.connect(_on_interaction_area_exited)
@@ -22,7 +20,6 @@ func _process(_delta: float) -> void:
 func _on_interaction_area_entered(area: Area3D) -> void:
 	if area.get_parent() is Player:
 		interaction_label.visible = true
-		interaction_label.text = label_text
 
 func _on_interaction_area_exited(area: Area3D) -> void:
 	if area.get_parent() is Player:
