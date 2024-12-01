9adec473a60faaaf979599d2e7f62e74a157baac
Tom <zailef.dev@hotmail.com>
2024-12-01 14:49:22 +0000

    Add placeholder sound for major arcana card pickups

--- scenes/pickups/major_arcana_card.gd
+++ scenes/pickups/major_arcana_card.gd
@@ -9,6 +9,7 @@ const ANIMATION_FADE_OUT: String = "fade_out"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var pickup_area: Area3D = $PickupArea
 @onready var card_model: CSGBox3D = $CardModel
+@onready var pickup_sound: AudioStreamPlayer = $PickupSound
 
 func _ready() -> void:
 	card_model.material.normal_texture = card_texture
@@ -19,6 +20,7 @@ func _on_pickup_area_body_entered(body: Node3D) -> void:
 		return
 	
 	animation_player.play(ANIMATION_FADE_OUT)
+	pickup_sound.play()
 	SignalManager.major_arcana_card_collected.emit(card_name)
 	await animation_player.animation_finished
 	queue_free()
