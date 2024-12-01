2fdf06700dc40bb106d04a70cfb5e2d377db4a7d
Tom <zailef.dev@hotmail.com>
2024-12-01 14:46:29 +0000

    Add logic to avoid playing the activation sound on the first spawn point

--- scenes/environment/platforms/spawn_card.gd
+++ scenes/environment/platforms/spawn_card.gd
@@ -8,18 +8,24 @@ class_name SpawnCard
 
 @export var is_active_spawn: bool = false
 
+static var activation_count: int = 0
+
 func _ready() -> void:
 	player_detection_area.body_entered.connect(_on_player_detection_area_body_entered)
 
 func _on_player_detection_area_body_entered(body: Node) -> void:
 	if not body is Player:
 		return
-
+	
 	is_active_spawn = true
 	card.material_overlay.set_shader_parameter("is_active", true)
-	spawn_activated_sound.play()
+
+	if activation_count > 0:
+		spawn_activated_sound.play()
 	
 	for child in get_tree().get_nodes_in_group("spawn_cards"):
 		if child != self:
 			child.is_active_spawn = false
 			child.card.material_overlay.set_shader_parameter("is_active", false)
+
+	activation_count += 1
